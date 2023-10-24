0852cf9eb7a57a6649d54f90edf88a64891e3416
Katie Shirley <shirleyk@mpfi.org>
2023-10-24 14:51:33 -0400

    added main and corrected my decoder function

--- Lab6.py
+++ Lab6.py
@@ -5,14 +5,35 @@ def encode (password):
 	encoded = ''
 	for i in password_str:
 		#temp = password_Str[i]
-		encoded[i] = int(password_str[i])+3
-	return encoded
+		encoded = int(password_st
 
 def decode (password)
 	password_str =str(password)
 	decoded = ''
 	for i in password_str:
-		decoded[i] = int(password_str[i]-3)
+		decoded = int(password_str[i]-3)
 	return decoded
 
+if __name__ == '__main__':
+   option = 0
+   while option !=3:
+	password = ''
+	print()
+	print('Menu\n-------------\n')
+	print('1. Encode')
+	print('2. Decode')
+	print('3. Quit')
+	pinrt()
+	option = int(input("Please enter an option: "))
+
+	if option == 1:
+		password = input("Please enter your password to encode: ")
+		encoded_pw = encode(password)
+		print("Your password has been encoded and stored!")
+	if option == 2:
+		print("The encoded password is " + encoded_pw + " and the original password is " + decode(encoded_pw))
+	if option == 3:
+		break
+
+	
 
